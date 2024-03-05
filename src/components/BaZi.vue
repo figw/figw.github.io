e84a618cd1c3dc2c632b6de6281844df334be3ba
figw <302750876@qq.com>
2024-03-05 16:41:41 +0800

    bugfix

--- src/components/BaZi.vue
+++ src/components/BaZi.vue
@@ -1,6 +1,20 @@
 <script setup>
 import { ref } from 'vue'
-import { CANG_GAN, CHANG_SHENG_MAP, KONG_WANG_MAP, NA_YIN_MAP, SHI_SHEN_MAP, TIAN_GAN, WU_XING_COLOR, WU_XING_MAP, YANG_GAN, YANG_ZHI, YIN_GAN, YIN_ZHI } from '../assets/constant.js'
+import {
+    CANG_GAN,
+    CHANG_SHENG_MAP,
+    KONG_WANG_MAP,
+    NA_YIN_MAP,
+    SHI_SHEN_MAP,
+    SHI_SHEN_SHORT_NAME,
+    TIAN_GAN,
+    WU_XING_COLOR,
+    WU_XING_MAP,
+    YANG_GAN,
+    YANG_ZHI,
+    YIN_GAN,
+    YIN_ZHI
+} from '../assets/constant.js'
 
 const nianGan = ref(), nianZhi = ref(), yueGan = ref(), yueZhi = ref(), riGan = ref(), riZhi = ref(), shiGan = ref(), shiZhi = ref()
 const buttonDialogVisible = ref(false)
@@ -10,6 +24,10 @@ const dialogOptions = ref()
 const dialogVisible = ref(false)
 const note = ref('')
 const noteDialogVisible = ref(false)
+
+function getColor(key) {
+    return 'color:' + WU_XING_COLOR[WU_XING_MAP[key]]
+}
 
 function randomOne() {
     const sample = (arr) => arr[Math.floor(Math.random() * arr.length)]
@@ -28,28 +46,28 @@ function randomOne() {
   <!--  干支  -->
   <el-row justify="center" style="color: #cccccc">
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
-      <p class="nmlFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[nianGan]]" @click="buttonDialogVisible=true">{{riGan && nianGan ? SHI_SHEN_MAP[riGan][nianGan] : '年'}}</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[nianGan]]" @click="dialogMark=1; dialogOptions=TIAN_GAN; dialogVisible=true">{{nianGan || '干'}}</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[nianZhi]]" @click="dialogMark=2; dialogOptions=YANG_GAN.indexOf(nianGan)>=0?YANG_ZHI:YIN_ZHI; nianGan&&(dialogVisible=true)">{{nianZhi || '支'}}</p>
-      <p class="smlFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[i]]" v-for="i in CANG_GAN[nianZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_MAP[riGan][i] : '')}}</p>
+      <p class="nmlFont" :style="getColor(nianGan)" @click="buttonDialogVisible=true">{{riGan && nianGan ? SHI_SHEN_SHORT_NAME[SHI_SHEN_MAP[riGan][nianGan]] : '年'}}</p>
+      <p class="bigFont" :style="getColor(nianGan)" @click="dialogMark=1; dialogOptions=TIAN_GAN; dialogVisible=true">{{nianGan || '干'}}</p>
+      <p class="bigFont" :style="getColor(nianZhi)" @click="dialogMark=2; dialogOptions=YANG_GAN.indexOf(nianGan)>=0?YANG_ZHI:YIN_ZHI; nianGan&&(dialogVisible=true)">{{nianZhi || '支'}}</p>
+      <p class="smlFont" :style="getColor(i)" v-for="i in CANG_GAN[nianZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_SHORT_NAME[SHI_SHEN_MAP[riGan][i]] : '')}}</p>
     </el-col>
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
-      <p class="nmlFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[yueGan]]" @click="buttonDialogVisible=true">{{riGan && yueGan ? SHI_SHEN_MAP[riGan][yueGan] : '月'}}</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[yueGan]]" @click="dialogMark=3; dialogOptions=TIAN_GAN; dialogVisible=true">{{yueGan || '干'}}</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[yueZhi]]" @click="dialogMark=4; dialogOptions=YANG_GAN.indexOf(yueGan)>=0?YANG_ZHI:YIN_ZHI; yueGan&&(dialogVisible=true)">{{yueZhi || '支'}}</p>
-      <p class="smlFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[i]]" v-for="i in CANG_GAN[yueZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_MAP[riGan][i] : '')}}</p>
+      <p class="nmlFont" :style="getColor(yueGan)" @click="buttonDialogVisible=true">{{riGan && yueGan ? SHI_SHEN_SHORT_NAME[SHI_SHEN_MAP[riGan][yueGan]] : '月'}}</p>
+      <p class="bigFont" :style="getColor(yueGan)" @click="dialogMark=3; dialogOptions=TIAN_GAN; dialogVisible=true">{{yueGan || '干'}}</p>
+      <p class="bigFont" :style="getColor(yueZhi)" @click="dialogMark=4; dialogOptions=YANG_GAN.indexOf(yueGan)>=0?YANG_ZHI:YIN_ZHI; yueGan&&(dialogVisible=true)">{{yueZhi || '支'}}</p>
+      <p class="smlFont" :style="getColor(i)" v-for="i in CANG_GAN[yueZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_SHORT_NAME[SHI_SHEN_MAP[riGan][i]] : '')}}</p>
     </el-col>
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
       <p class="nmlFont" :style="'color:'+(riGan&&'#555555')" @click="buttonDialogVisible=true">日主</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[riGan]]" @click="dialogMark=5; dialogOptions=TIAN_GAN; dialogVisible=true">{{riGan || '干'}}</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[riZhi]]" @click="dialogMark=6; dialogOptions=YANG_GAN.indexOf(riGan)>=0?YANG_ZHI:YIN_ZHI; riGan&&(dialogVisible=true)">{{riZhi || '支'}}</p>
-      <p class="smlFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[i]]" v-for="i in CANG_GAN[riZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_MAP[riGan][i] : '')}}</p>
+      <p class="bigFont" :style="getColor(riGan)" @click="dialogMark=5; dialogOptions=TIAN_GAN; dialogVisible=true">{{riGan || '干'}}</p>
+      <p class="bigFont" :style="getColor(riZhi)" @click="dialogMark=6; dialogOptions=YANG_GAN.indexOf(riGan)>=0?YANG_ZHI:YIN_ZHI; riGan&&(dialogVisible=true)">{{riZhi || '支'}}</p>
+      <p class="smlFont" :style="getColor(i)" v-for="i in CANG_GAN[riZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_SHORT_NAME[SHI_SHEN_MAP[riGan][i]] : '')}}</p>
     </el-col>
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
-      <p class="nmlFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[shiGan]]" @click="buttonDialogVisible=true">{{riGan && shiGan ? SHI_SHEN_MAP[riGan][shiGan] : '时'}}</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[shiGan]]" @click="dialogMark=7; dialogOptions=TIAN_GAN; dialogVisible=true">{{shiGan || '干'}}</p>
-      <p class="bigFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[shiZhi]]" @click="dialogMark=8; dialogOptions=YANG_GAN.indexOf(shiGan)>=0?YANG_ZHI:YIN_ZHI; shiGan&&(dialogVisible=true)">{{shiZhi || '支'}}</p>
-      <p class="smlFont" :style="'color:'+WU_XING_COLOR[WU_XING_MAP[i]]" v-for="i in CANG_GAN[shiZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_MAP[riGan][i] : '')}}</p>
+      <p class="nmlFont" :style="getColor(shiGan)" @click="buttonDialogVisible=true">{{riGan && shiGan ? SHI_SHEN_SHORT_NAME[SHI_SHEN_MAP[riGan][shiGan]] : '时'}}</p>
+      <p class="bigFont" :style="getColor(shiGan)" @click="dialogMark=7; dialogOptions=TIAN_GAN; dialogVisible=true">{{shiGan || '干'}}</p>
+      <p class="bigFont" :style="getColor(shiZhi)" @click="dialogMark=8; dialogOptions=YANG_GAN.indexOf(shiGan)>=0?YANG_ZHI:YIN_ZHI; shiGan&&(dialogVisible=true)">{{shiZhi || '支'}}</p>
+      <p class="smlFont" :style="getColor(i)" v-for="i in CANG_GAN[shiZhi] || []">{{i + (riGan ? ' · '+SHI_SHEN_SHORT_NAME[SHI_SHEN_MAP[riGan][i]] : '')}}</p>
     </el-col>
   </el-row>
   <!--  长生 & 空亡 & 纳音  -->
@@ -57,22 +75,22 @@ function randomOne() {
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
       <p class="smlFont" v-if="riGan&&nianZhi" style="color:#555555">{{CHANG_SHENG_MAP[riGan][nianZhi]}}</p>
       <p class="smlFont" v-if="nianGan&&nianZhi" style="color:#555555">{{KONG_WANG_MAP[nianGan][nianZhi] + '空'}}</p>
-      <p class="smlFont" v-if="nianGan&&nianZhi" :style="'color:'+WU_XING_COLOR[NA_YIN_MAP[nianGan][nianZhi][2]]">{{NA_YIN_MAP[nianGan][nianZhi]}}</p>
+      <p class="smlFont" v-if="nianGan&&nianZhi" :style="getColor(NA_YIN_MAP[nianGan][nianZhi])">{{NA_YIN_MAP[nianGan][nianZhi]}}</p>
     </el-col>
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
       <p class="smlFont" v-if="riGan&&yueZhi" style="color:#555555">{{CHANG_SHENG_MAP[riGan][yueZhi]}}</p>
       <p class="smlFont" v-if="yueGan&&yueZhi" style="color:#555555">{{KONG_WANG_MAP[yueGan][yueZhi] + '空'}}</p>
-      <p class="smlFont" v-if="yueGan&&yueZhi" :style="'color:'+WU_XING_COLOR[NA_YIN_MAP[yueGan][yueZhi][2]]">{{NA_YIN_MAP[yueGan][yueZhi]}}</p>
+      <p class="smlFont" v-if="yueGan&&yueZhi" :style="getColor(NA_YIN_MAP[yueGan][yueZhi])">{{NA_YIN_MAP[yueGan][yueZhi]}}</p>
     </el-col>
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
       <p class="smlFont" v-if="riGan&&riZhi" style="color:#555555">{{CHANG_SHENG_MAP[riGan][riZhi]}}</p>
       <p class="smlFont" v-if="riGan&&riZhi" style="color:#555555">{{KONG_WANG_MAP[riGan][riZhi] + '空'}}</p>
-      <p class="smlFont" v-if="riGan&&riZhi" :style="'color:'+WU_XING_COLOR[NA_YIN_MAP[riGan][riZhi][2]]">{{NA_YIN_MAP[riGan][riZhi]}}</p>
+      <p class="smlFont" v-if="riGan&&riZhi" :style="getColor(NA_YIN_MAP[riGan][riZhi])">{{NA_YIN_MAP[riGan][riZhi]}}</p>
     </el-col>
     <el-col :xs="6" :sm="6" :lg="4" :xl="2">
       <p class="smlFont" v-if="riGan&&shiZhi" style="color:#555555">{{CHANG_SHENG_MAP[riGan][shiZhi]}}</p>
       <p class="smlFont" v-if="shiGan&&shiZhi" style="color:#555555">{{KONG_WANG_MAP[shiGan][shiZhi] + '空'}}</p>
-      <p class="smlFont" v-if="shiGan&&shiZhi" :style="'color:'+WU_XING_COLOR[NA_YIN_MAP[shiGan][shiZhi][2]]">{{NA_YIN_MAP[shiGan][shiZhi]}}</p>
+      <p class="smlFont" v-if="shiGan&&shiZhi" :style="getColor(NA_YIN_MAP[shiGan][shiZhi])">{{NA_YIN_MAP[shiGan][shiZhi]}}</p>
     </el-col>
   </el-row>
   <!--  干支选择面板  -->
